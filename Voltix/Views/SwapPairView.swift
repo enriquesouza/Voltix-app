974ac4a72ad980632e2efa4fb1193618e4bd4539
Dolloong <brownwithmail@gmail.com>
2024-02-01 06:56:15 +0300

    feat: implement Swap Pair page same as Pair Page

---
+++ Voltix/Views/SwapPairView.swift
@@ -0,0 +1,50 @@
+ //
+//  SwapPairView.swift
+//  Voltix
+//
+//  Created by Mac on 31.01.2024.
+//
+
+import SwiftUI
+
+struct SwapPairView: View {
+    var body: some View {
+        VStack {
+          HeaderView(
+            rightIcon: "QuestionMark",
+            leftIcon: "BackArrow",
+            head: "PAIR",
+            leftAction: {},
+            rightAction: {}
+          )
+          Text("VERIFY ALL DETAILS")
+            .font(Font.custom("Montserrat", size: 24).weight(.medium))
+            .lineSpacing(36)
+            .foregroundColor(.black)
+          Spacer().frame(height: 80)
+          Text("iPhone 15 Pro\nMatt’s iPhone")
+            .font(Font.custom("Montserrat", size: 24).weight(.medium))
+            .lineSpacing(12)
+            .foregroundColor(.black)
+          Spacer().frame(height: 20)
+          Text("42")
+            .font(Font.custom("Montserrat", size: 80).weight(.light))
+            .lineSpacing(120)
+            .foregroundColor(.black)
+          Spacer()
+          WifiBar()
+          Spacer().frame(height: 70 )
+        }
+        .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity,
+            alignment: .top
+        )
+    }
+} 
+
+#Preview {
+    SwapPairView()
+}
